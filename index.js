9ddc026c8a59eb59f8f4f99ae4eeb2c5ed0f5517
Calypso Leonard <calypsorleonard@gmail.com>
2022-02-25 10:06:37 -0500

    commit tree 8b090cf3341ea3e3655fea5ee53f6f1e74f8c8d6

--- index.js
+++ index.js
@@ -12,5 +12,37 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const doubleArray = tutorials.map(spaceArray)
+  const cappedArray = []
+  //const again = doubleArray.map(feedEachTitle)
+  //console.log(again)
+  //console.log('DoubleArray ',doubleArray[0])
+  //const testArray = doubleArray[0].map(capitalizeFirstLetter)
+  //console.log("testArray ", testArray)
+  for (let title of doubleArray){
+    let temp = title.map(capitalizeFirstLetter)
+    temp = temp.join(" ")
+    cappedArray.push(temp)
+    // console.log(cappedArray)
+    // //cappedArray.push(title.map(capitalizeFirstLetter))
+  }
+  return cappedArray
+  //console.log(cappedArray[0])
+  //console.log(doubleArray[0].map(capitalizeFirstLetter))
+  //tutorials.map(title => title.map(spaceArray(tutorials)))
+  //
+  //tutorials.map(spaceArray)[0].map(capitalizeFirstLetter).join(" ")
 }
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+// function feedEachTitle(array){
+//   return array
+// }
+
+function spaceArray(array){
+  const newArray = array.split(" ")
+  return newArray
+}
+
